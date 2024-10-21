694daba51c15c7e325509c3f4bb4287adbd5b54d
Marcus Vynicius <viniciusfla1999@gmail.com>
2024-10-22 00:16:35 +0100

    first code in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Botao';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Calculator</h1>
+    <div>
+      <label>First Number</label>
+      <input value={firstNumber} onChange={e => setFirstNumber(Number(e.target.value))} />
+
+      <label>Second Number</label>
+      <input value={secondNumber} onChange={e => setSecondNumber(Number(e.target.value))} />
+
+      <span>Result: {result}</span>
     </div>
+    <button onClick={() => setResult(firstNumber + secondNumber)}>+</button>
+    <button onClick={() => setResult(firstNumber - secondNumber)}>-</button>
+    <button onClick={() => setResult(firstNumber * secondNumber)}>*</button>
+    <button onClick={() => setResult(firstNumber / secondNumber)}>/</button>
+    </>
   );
 }
 
---
+++ src/Botao.js
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button onClick= {() => alert("Clicado")}>Me clique</button>
+};
+
+export default Button;
